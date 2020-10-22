eb93974cbc110f6158dab56f5fc44ff20998161e
Moises Velasquez <mvelasquez@pictoric.com>
2020-10-22 00:06:12 -0500

    my first component Counter

---
+++ src/components/Counter.js
@@ -0,0 +1,16 @@
+import React, { useState, Fragment } from 'react'
+
+const Counter = () => {
+    const [number, setNumber] = useState(0);
+    const increase = () => {
+        setNumber(number + 1)
+    }
+    return (
+        <Fragment>
+            <h3>My first component {number}</h3>
+            <button onClick={increase}>Increase</button>
+        </Fragment>
+    );
+}
+ 
+export default Counter;
