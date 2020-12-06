c8f51c5c72c22c857454e98d598b35ee8196d2d7
Moises Velasquez <mvelasquez@pictoric.com>
2020-12-05 20:02:04 -0500

    example2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import Example1 from './components/Example1';
+// import Example1 from './components/Example1';
 // import Counter from './components/Counter';
 // import ListNumber from './components/ListNumber';
 // import Temperature from './components/temperature';
+import Example2 from './components/Example2';
 import Form from './components/Form';
 
 function App() {
   return (
     <div className="mt-5">
-      {/* <h1>Hola mundo..!</h1>
-      <Counter></Counter>
+      <h1>Hola mundo..!</h1>
+      {/* <Counter></Counter>
       <Temperature />
       <ListNumber />
-	    <Form /> */}
-      <Example1/>
+	    <Form />
+      <Example1/> */}
+      <Example2/>
     </div>
   );
 }
