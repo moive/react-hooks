791cafefbaefb78bc3563c1dd2c2c9d111f3580f
Moises Velasquez <mvelasquez@pictoric.com>
2020-12-22 22:28:21 -0500

    application CRUD

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import React from 'react';
-import Commetary from './components/Commentary';
-import Greeting from './components/gretting';
+import React, {useState} from 'react';
+import UserTable from './components/UserTable';
+import { v4 as uuidv4 } from 'uuid'
+import AddUserForm from './components/AddUserForm';
+import EditUserForm from './components/EditUserForm';
 
 
 function App() {
 
-  const people = {
-    name:'Daniela',
-    urlImage:'https://via.placeholder.com/64',
-    text:'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+  const usersData = [
+    {id:uuidv4(), name: 'Tania', username:'floppydiskette'},
+    {id:uuidv4(), name: 'Craig', username:'siliconeidolon'},
+    {id:uuidv4(), name: 'Ben', username:'benisphere'},
+  ];
+
+  const [users, setusers] = useState(usersData);
+
+  // add user
+  const addUser = (user)=> {
+    user.id = uuidv4();
+    setusers([
+      ...users,
+      user
+    ]);
+  };
+
+  // delete user
+  const deleteUser = id => {
+    setusers(users.filter(user => user.id !== id));
+    console.log(users);
   }
+
+  const [editing, setEditing] = useState(false);
+
+  const [currentUser, setCurrentUser] = useState({
+    id:null, name:'', username:''
+  });
+
+  const editRow = (user)=>{
+    setEditing(true);
+    setCurrentUser({
+      id:user.id,
+      name: user.name,
+      username: user.username
+    })
+
+  };
+
+  const updateUser = (id, updatedUser)=>{
+    setEditing(false);
+    setusers(users.map(user => (user.id === id ? updatedUser : user)));
+  }
+
   return (
-    <div className="mt-5">
-      <h1>Props in components</h1>
-      <Greeting person = "Moises" age={38} />
-      <Greeting person = "Pedro" age={26} />
-      <Commetary people={people}/>
-      <Commetary people={people}/>
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          {
+            editing ? (
+              <div>
+                <h2>Edit User</h2>
+                <EditUserForm
+                  currentUser={currentUser}
+                  updateUser={updateUser}
+                />
+              </div>
+            ) : (
+              <div>
+                <h2>Add User</h2>
+                <AddUserForm addUser={addUser} />
+              </div>
+            )
+          }
+        </div>
+        <div className="flex-large">
+          <h2>View Users</h2>
+          <UserTable
+            users={users} 
+            deleteUser={deleteUser}
+            editRow = {editRow}
+          />
+        </div>
+      </div>
     </div>
-  );
+  ); 
 }
 
 export default App;
