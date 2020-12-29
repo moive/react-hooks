75cd44d6a1a9674f362618ca0ddaf4478e8d0612
Moises Velasquez <mvelasquez@pictoric.com>
2020-12-28 22:20:12 -0500

    routes in react

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import { v4 as uuidv4 } from 'uuid'
 import AddUserForm from './components/AddUserForm';
 import EditUserForm from './components/EditUserForm';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+
 
 function App() {
 
@@ -52,37 +60,58 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <h1>CRUD App with Hooks</h1>
-      <div className="flex-row">
-        <div className="flex-large">
-          {
-            editing ? (
-              <div>
-                <h2>Edit User</h2>
-                <EditUserForm
-                  currentUser={currentUser}
-                  updateUser={updateUser}
+    <Router>
+      <div className="container">
+        <div className="btn-group mt-5">
+          <Link to="/" className="btn btn-dark">Home</Link>
+          <Link to="/about" className="btn btn-dark">About Us</Link>
+          <Link to="/contact" className="btn btn-dark">Contact</Link>
+          <NavLink to="/crud" className="btn btn-dark" activeClassName="active">Crud</NavLink>
+        </div>
+        <Switch>
+          <Route path="/" exact>
+            <h2>Home</h2>
+          </Route>
+          <Route path="/crud">
+            <h1>CRUD App with Hooks</h1>
+            <div className="flex-row">
+              <div className="flex-large">
+                {
+                  editing ? (
+                    <div>
+                      <h2>Edit User</h2>
+                      <EditUserForm
+                        currentUser={currentUser}
+                        updateUser={updateUser}
+                      />
+                    </div>
+                  ) : (
+                    <div>
+                      <h2>Add User</h2>
+                      <AddUserForm addUser={addUser} />
+                    </div>
+                  )
+                }
+              </div>
+              <div className="flex-large">
+                <h2>View Users</h2>
+                <UserTable
+                  users={users} 
+                  deleteUser={deleteUser}
+                  editRow = {editRow}
                 />
               </div>
-            ) : (
-              <div>
-                <h2>Add User</h2>
-                <AddUserForm addUser={addUser} />
-              </div>
-            )
-          }
-        </div>
-        <div className="flex-large">
-          <h2>View Users</h2>
-          <UserTable
-            users={users} 
-            deleteUser={deleteUser}
-            editRow = {editRow}
-          />
-        </div>
+            </div>
+          </Route>
+          <Route path="/about">
+            <h2>About us</h2>
+          </Route>
+          <Route path="/contact">
+            <h2>Contact</h2>
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   ); 
 }
 
