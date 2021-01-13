0023efed80090a1e31a885f93250768e06e0bcde
Moises Velasquez <mvelasquez@pictoric.com>
2021-01-13 18:53:34 -0500

    custom style button

--- src/components/MaterialUi.jsx
+++ src/components/MaterialUi.jsx
@@ -1,7 +1,20 @@
-import { Button, Icon, Typography } from '@material-ui/core';
+import { Button, Icon, makeStyles, Typography } from '@material-ui/core';
 import React from 'react'
 
+const useStyle = makeStyles({
+    custonBtn:{
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+    }
+});
+
 const MaterialUi = () => {
+    const classes = useStyle();
     const ClickMe = ()=> {
         console.log("Click me...!");
     }
@@ -16,6 +29,10 @@ const MaterialUi = () => {
                 onClick={()=> ClickMe()}
                 startIcon={<Icon>start</Icon>}
             >Click me!</Button>
+            <Typography variant="body2" paragraph color="initial">Testing...!</Typography>
+            <Button className={classes.custonBtn}>
+                My customize button
+            </Button>
         </div>
     )
 }
